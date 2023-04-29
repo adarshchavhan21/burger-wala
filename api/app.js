bccab5dbd628739482d78ef922ca810dd8e5f08e
Adarsh Chavhan <130956407+adarshchavhan21@users.noreply.github.com>
2023-04-29 21:25:55 +0530

    small fix

--- api/app.js
+++ api/app.js
@@ -35,12 +35,13 @@ app.use(session({
     name: 'access_token',
     cookie: {
         secure: process.env.NODE_ENV === 'development' ? false:true,
-        httpOnly: process.env.NODE_ENV === 'development' ? true:false,
+        httpOnly: process.env.NODE_ENV === 'development' ? false:true,
         sameSite: process.env.NODE_ENV === 'development' ? false:'none'
     }
 }));
 app.use(passport.authorize('session'));
 app.use(passport.initialize());
+app.enable('trust proxy')
 
 // routes
 app.use('/api/users', userRoutes);
